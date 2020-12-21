e1f56f5e047ecb10805d836007f065ea67112424
Baris Ucakturk <baris.ucakturk@adesso.com.tr>
2020-12-21 23:34:53 +0300

    Checking diff followers for already created user implemented

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const {get} = require('axios');
-const mongoose = require('mongoose');
 const User = require('./models/User');
 const Follower = require('./models/Follower');
 const {connectToServer} = require('./mongoose');
 
 const TOKEN_URL = "https://open.spotify.com/get_access_token?reason=transport&productType=web_player";
 const PROFILE_ID = "11101586339"
-// getAccessToken();
 const DB_URL = "mongodb://localhost:27017/test";
 connectToServer(DB_URL)
 
-checkUser(PROFILE_ID);
-// .then((user) => {
-//     console.log(user)
-//     return getAccessToken()
-// })
-// // TODO: If user does not exist, save all followers. Else fetch and compare.
-// .then(accessToken => {
-//     console.log(`Got access token`, accessToken)
-//     return requestFollowers(accessToken, PROFILE_ID)
-// })
-// .then(followers => {
-//     return followers.splice(0, 5);
-// })
-// .then(followers => {
-//
-// })
+checkUser(PROFILE_ID)
+    .then(user => {
+        if (user) {
+            let previousFollowerIds = user.followers;
+            return getAccessToken()
+                .then(accessToken => {
+                    console.log(`Got access token for check difference`, accessToken)
+                    return requestFollowers(accessToken, PROFILE_ID)
+                })
+                .then(currentFollowers => {
+                    fetchFollowers(previousFollowerIds)
+                        .then(previousFollowers => {
+                            let currentFollowerNames = currentFollowers.map(currentFollower => currentFollower.name);
+                            let previousFollowerNames = previousFollowers.map(previousFollower => previousFollower.name);
+                            let newFollowers = currentFollowerNames.filter(x => !previousFollowerNames.includes(x));
+                            let usersToLeaveFollowing = previousFollowerNames.filter(x => !currentFollowerNames.includes(x));
+                            let followersDiff = new FollowersDiff(newFollowers, usersToLeaveFollowing);
+                            console.log("Difference:", followersDiff)
+                        })
+                })
+        }
+    });
 
 function checkUser(userId) {
     console.log('Checking User')
 
-    User.findOne({id: userId})
+    return User.findOne({id: userId})
         .then(user => {
             if (user) {
                 console.log('User exists!')
@@ -41,22 +45,39 @@ function checkUser(userId) {
                 return user;
             } else {
                 console.log('Creating new user')
-                getAccessToken()
-                    .then(accessToken => {
-                        console.log(`Got access token`, accessToken)
-                        return requestFollowers(accessToken, PROFILE_ID)
-                    }).then(followers => {
-                    return saveFollowers(followers)
-
-                }).then(followers => {
-                    return new User({
-                        id: userId,
-                        followers: followers
-                    })
-                }).then(user => user.save())
-
+                createUser(userId)
             }
         })
+}
+
+function createUser(userId) {
+    getAccessToken()
+        .then(accessToken => {
+            console.log(`Got access token`, accessToken)
+            return requestFollowers(accessToken, PROFILE_ID)
+        }).then(followers => {
+        return saveFollowers(followers)
+    }).then(followers => {
+        return new User({
+            id: userId,
+            followers: followers
+        })
+    }).then(user => {
+        user.save()
+    });
+}
+
+async function getAccessToken() {
+    const {data} = await get(TOKEN_URL)
+    return data.accessToken;
+}
+
+function fetchFollowers(previousFollowers) {
+    let promises = [];
+    for (follower of previousFollowers) {
+        promises.push(Follower.findById(follower._id))
+    }
+    return Promise.all(promises);
 }
 
 function saveFollowers(followers) {
@@ -76,11 +97,6 @@ function saveFollowers(followers) {
     return Promise.all(promises);
 }
 
-async function getAccessToken() {
-    const {data} = await get(TOKEN_URL)
-    return data.accessToken;
-}
-
 async function requestFollowers(accessToken, userId) {
     const followersURL = `https://spclient.wg.spotify.com/user-profile-view/v3/profile/${userId}/followers?market=TR`
 
@@ -89,4 +105,9 @@ async function requestFollowers(accessToken, userId) {
             Authorization: `Bearer ${accessToken}`
         }
     }).then(response => response.data.profiles)
+}
+
+function FollowersDiff(newFollowers, usersToLeaveFollowing) {
+    this.newFollowers = newFollowers;
+    this.usersToLeaveFollowing = usersToLeaveFollowing;
 }
