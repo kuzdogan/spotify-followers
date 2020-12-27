6ae844daf63456ad5100334d1ad3ed88ec19da4a
Baris Ucakturk <baris.ucakturk@gmail.com>
2020-12-27 19:30:14 +0300

    updated get followers controller path

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,51 +4,63 @@ const TOKEN_URL = "https://open.spotify.com/get_access_token?reason=transport&pr
 
 export default class Homepage extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      followers: [],
-      userId: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            followers: [],
+        }
+        this.userId = 11101586339 // TODO: Dynamically handle userId inside state
     }
-    this.userId = 11101586339 // TODO: Dynamically handle userId inside state
-  }
 
-  handleIncrement = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }))
-  }
-
-
-  componentDidMount() {
-    // TODO: fetch all followers from the server endpoint. Then assign followers into the state.
-    // TODO: If input is dynamic fetch followers after clicking the button.
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <div> Loading </div>
-      )
+    handleIncrement = () => {
+        this.setState(prevState => ({count: prevState.count + 1}))
     }
-    return (
-      // TODO: if isloading == false render each follower in Follower component.
-      // {
-      //   this.followers.forEach(follower => {
-      //     <Follower user={follwer} />
-      //   })
-      // }
-      <div>
-        <div>
-          HELLO
-        </div>
-        <div>
-          {/* Followers: <br /> */}
-          {/* {this.state.followers} */}
-          Count: {this.state.count}
-          <button type='button' onClick={this.handleIncrement}>Increment</button>
-          <input onChange={this.handleUserIdChange}> </input>
-        </div>
-      </div>
-    )
-  }
+
+
+    componentDidMount() {
+        fetch(`http://localhost:3000/user/${this.userId}/followers`)
+            .then(response => response.json())
+            .then(data => {
+                    this.setState({
+                        isLoaded: true,
+                        followers: data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                })
+        // TODO: If input is dynamic fetch followers after clicking the button.
+    }
+
+    render() {
+        if (!this.state.isLoaded) {
+            return (
+                <div> Please wait.. Loading </div>
+            )
+        }
+        return (
+            // TODO: if isloading == false render each follower in Follower component.
+            // {
+            //   this.followers.forEach(follower => {
+            //     <Follower user={follwer} />
+            //   })
+            // }
+            <div>
+                <div>
+                    HELLO
+                </div>
+                <div>
+                    {/* Followers: <br /> */}
+                    Followers: {this.state.followers.map(follower => <div>{follower.name}</div>)}
+                    Count: {this.state.count}
+                    <button type='button' onClick={this.handleIncrement}>Increment</button>
+                    {/*<input onChange={this.handleUserIdChange}> </input>*/}
+                </div>
+            </div>
+        )
+    }
 }
